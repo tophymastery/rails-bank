d9d3b6949ccfab94c3d278106dcfc35a81f1a560
Akadet Mathurapoj <akadet85@gmail.com>
2017-08-05 17:27:55 +0700

    add modal for create ‘new transaction’

---
+++ app/assets/javascripts/bank_accounts/show.js
@@ -0,0 +1,29 @@
+var Show = (function() {
+  var $btnNewTransaction;
+  var $modalTransaction;
+  var $btnSave;
+  var $inputAmount;
+  var $selectTransactionType;
+
+  var fetchElements = function() {
+    $btnNewTransaction = $("#btn-new-transaction");
+    $modalTransaction = $('#modal-transaction');
+    $btnSave = $("#btn-save");
+    $inputAmount = $("#input-amount");
+    $selectTransactionType = $("#select-transaction-type");
+  };
+
+  var initializeEvents = function() {
+    $btnNewTransaction.on("click", function(){
+      $modalTransaction.modal("show");
+    });
+  };
+
+  var init = function() {
+    fetchElements();
+    initializeEvents();
+  };
+  return {
+    init: init
+  };
+})();
