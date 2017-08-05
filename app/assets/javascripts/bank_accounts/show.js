246aaf423fbd83f9eb5ad2dc62a80cf3445fb9bf
Akadet Mathurapoj <akadet85@gmail.com>
2017-08-05 18:45:26 +0700

    add ajax to call transaction api

--- app/assets/javascripts/bank_accounts/show.js
+++ app/assets/javascripts/bank_accounts/show.js
@@ -4,6 +4,11 @@ var Show = (function() {
   var $btnSave;
   var $inputAmount;
   var $selectTransactionType;
+  var $parameters;
+  var $notification;
+
+  var bankAccountId;
+  var url = "/api/v1/bank_accounts/new_transaction";
 
   var fetchElements = function() {
     $btnNewTransaction = $("#btn-new-transaction");
@@ -11,11 +16,53 @@ var Show = (function() {
     $btnSave = $("#btn-save");
     $inputAmount = $("#input-amount");
     $selectTransactionType = $("#select-transaction-type");
+    $parameters = $("#parameters");
+    $notification = $(".notification");
+
+    bankAccountId = $parameters.data("bank-account-id");
+  };
+
+  var disableControls = function() {
+    $btnSave.prop("disabled", true);
+    $inputAmount.prop("disabled", true);
+    $selectTransactionType.prop("disabled", true);
+  };
+
+  var enableControls = function() {
+    $btnSave.prop("disabled", false);
+    $inputAmount.prop("disabled", false);
+    $selectTransactionType.prop("disabled", false);
   };
 
   var initializeEvents = function() {
-    $btnNewTransaction.on("click", function(){
+    $btnNewTransaction.on("click", function() {
       $modalTransaction.modal("show");
+    });
+
+    $btnSave.on("click", function() {
+      var amount = $inputAmount.val();
+      var transactionType = $selectTransactionType.val();
+      disableControls();
+
+      $notification.html("");
+
+      $.ajax({
+        url: url,
+        method: 'POST',
+        dataType: 'json',
+        data: {
+          amount: amount,
+          transaction_type: transactionType,
+          bank_account_id: bankAccountId
+        },
+        success: function(response) {
+          window.location.href = "/bank_accounts/" + bankAccountId;
+        },
+        error: function(response) {
+          $notification.html(JSON.parse(response.responseText).errors.join());
+          enableControls();
+        }
+      });
     });
   };
 
