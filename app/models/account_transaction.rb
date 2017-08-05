f94df12432366f8f98c835cfbfbd1bf11429d172
Akadet Mathurapoj <akadet85@gmail.com>
2017-08-05 15:00:18 +0700

    add model validation for client, bank_account and account_transaction

--- app/models/account_transaction.rb
+++ app/models/account_transaction.rb
@@ -1,3 +1,18 @@
 class AccountTransaction < ApplicationRecord
   belongs_to :bank_account
+
+  TRANSACTION_TYPES = ["withdraw", "deposit"]
+
+  validates :bank_account, presence: true
+  validates :amount, presence: true, numbericality: true
+  validates :transaction_type, presence: true, inclusion: { in: TRANSACTION_TYPES }
+  validates :transaction_number, presence: true, uniquebess: true
+
+  before_validation :load_defaults
+
+  def load_defaults
+    if self.new_record?
+      self.transaction_number = SecureRandom.uuid
+    end
+  end
 end
--- app/models/bank_account.rb
+++ app/models/bank_account.rb
@@ -1,3 +1,19 @@
 class BankAccount < ApplicationRecord
   belongs_to :client
+
+  validates :client, presense: true
+  validates :account_number, presense: true, uniqueness: true
+  validates :balance, presense: true, numbericality: true
+
+  before_validation :load_defaults
+
+  def load_defaults
+    if self.new_record?
+      self.balance = 0.00
+    end
+  end
+
+  def to_s
+    account_number
+  end
 end
