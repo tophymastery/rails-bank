34ad051b1546ef298d95847f33a03055b8356ec2
Akadet Mathurapoj <akadet85@gmail.com>
2017-08-05 15:52:16 +0700

    create bank_account view table

---
+++ app/controllers/bank_accounts_controller.rb
@@ -0,0 +1,10 @@
+class BankAccountsController < ApplicationController
+
+  def index
+    @bank_accounts = BankAccount.all
+  end
+
+  def show
+    @bank_account = BankAccount.find(params[:id])
+  end
+end
