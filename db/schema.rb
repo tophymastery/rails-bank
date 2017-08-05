893d6207b92e24f763d0d293a1fd439f7a5e927b
Akadet Mathurapoj <akadet85@gmail.com>
2017-08-05 14:57:18 +0700

    add transaction_number to table account_transaction

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170805073911) do
+ActiveRecord::Schema.define(version: 20170805075445) do
 
   create_table "account_transactions", force: :cascade do |t|
     t.decimal "amount"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20170805073911) do
     t.integer "bank_account_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "transaction_number"
     t.index ["bank_account_id"], name: "index_account_transactions_on_bank_account_id"
   end
 
